3950542f24258676e94fcaee067d64619ad985af
sram055 <41504162+sram055@users.noreply.github.com>
2020-05-31 19:03:14 +0530

    Update xmlpy.py

--- xmlpy.py
+++ xmlpy.py
@@ -3,7 +3,7 @@ from collections import OrderedDict
 import lxml.etree as ET
 
 # BUILD NESTED ID DICTIONARY FROM CSV
-with open("Input.csv", 'r') as f:
+with open("input.csv") as f:
     reader = csv.DictReader(f)      
 
     id_dct = OrderedDict({})
